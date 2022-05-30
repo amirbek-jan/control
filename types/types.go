2bd014c806bc86c285e2b3407289c922a6e09fce
amirbek-jan <mahmudiamirbek@gmail.com>
2022-05-30 18:12:37 +0500

    Added go.mod and types.go

---
+++ types/types.go
@@ -0,0 +1,31 @@
+package types
+
+type Money int64
+
+type PAN string
+
+type Currency string
+
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+type Color string
+
+const (
+	Shit   Color = "SHIT"
+	Silver Color = "Silver"
+	Gold   Color = "Gold"
+)
+
+type Card struct {
+	ID       int
+	PAN      PAN
+	Balance  Money
+	Currency Currency
+	Color    Color
+	Name     string
+	Active   bool
+}
